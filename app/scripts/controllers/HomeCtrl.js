71a18ec4eae2b55245ac0ef977fae591447e1179
Robert Marsh <nhe6dc@gmail.com>
2017-11-17 21:35:51 -0500

    check 3

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,10 +1,18 @@
 (function() {
-    function HomeCtrl(Room) {
-       this.rooms = Room.all;
-       console.log(this.rooms)
-    }
+    function HomeCtrl(Room, $uibModal) {
+        var home = this;
+        home.chatRoomArray = Room.all;
+
+        home.openNewRoomModal = function() {
+        	var modalInstance = $uibModal.open({
+        		templateUrl: '/templates/new_room_modal.html',
+        		controller: 'ModalCtrl',
+        		controllerAs: 'modal'
+        	});
+        };
+    };
 
     angular
         .module('blocChat')
-        .controller('HomeCtrl', ['Room', HomeCtrl]);
+        .controller('HomeCtrl', ['Room', '$uibModal', HomeCtrl]);
 })();
